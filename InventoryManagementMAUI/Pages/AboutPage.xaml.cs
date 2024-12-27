b311aee36614f622f24c0018f93db758ac727767
lextrack <arielmunozg97@gmail.com>
2024-12-26 22:53:50 -0300

    Added donation link inside the about

--- InventoryManagementMAUI/Pages/AboutPage.xaml.cs
+++ InventoryManagementMAUI/Pages/AboutPage.xaml.cs
@@ -5,11 +5,13 @@ namespace InventoryManagementMAUI.Pages;
 public partial class AboutPage : ContentPage
 {
     public ICommand OpenGitHubCommand { get; }
+    public ICommand OpenPayPalCommand { get; }
 
     public AboutPage()
     {
         InitializeComponent();
         OpenGitHubCommand = new Command(async () => await OpenGitHub());
+        OpenPayPalCommand = new Command(async () => await OpenPayPal());
         BindingContext = this;
     }
 
@@ -18,6 +20,19 @@ public partial class AboutPage : ContentPage
         try
         {
             Uri uri = new Uri("https://github.com/lextrack/InventoryManagementMAUI");
+            await Browser.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error", "Could not open the link.", "OK");
+        }
+    }
+
+    private async Task OpenPayPal()
+    {
+        try
+        {
+            Uri uri = new Uri("https://www.paypal.me/lextrack");
             await Browser.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
         }
         catch (Exception ex)
